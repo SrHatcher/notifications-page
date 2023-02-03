d896eb59832b126f47e925dee7c7fa4572e5da44
SrHatcher <erickomaldonado11@gmail.com>
2023-02-02 18:16:41 -0600

    adding styles to the html tags, doing just one notification, I'll insert the other ones via JS

---
+++ data.js
@@ -0,0 +1,72 @@
+const data = [
+    {
+        name: "Mark Webber",
+        profilePic: './assets/images/avatar-mark-webber.webp',
+        reason: "reacted to your recent post",
+        link: "My first tournament today!",
+        date: "1m ago",
+        relatedImage: null,
+        message: null,
+        newState: true
+    },
+    {
+        name: "Angela Gray",
+        profilePic: './assets/images/avatar-angela-gray.webp',
+        reason: "followed you",
+        link: null,
+        date: "5m ago",
+        relatedImage: null,
+        message: null,
+        newState: true
+    },
+    {
+        name: "Jacob Thompson",
+        profilePic: './assets/images/avatar-jacob-thompson.webp',
+        reason: "has joined your group",
+        link: "Chess Club",
+        date: "1 day ago",
+        relatedImage: null,
+        message: null,
+        newState: true
+    },
+    {
+        name: "Rizky Hasanuddin",
+        profilePic: './assets/images/avatar-rizky-hasanuddin.webp',
+        reason: "sent you a private message",
+        link: null,
+        date: "5 days ago",
+        relatedImage: null,
+        message: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game",
+        newState: false
+    },
+    {
+        name: "Kimberly Smith",
+        profilePic: './assets/images/avatar-kimberly-smith.webp',
+        reason: "commented on  your picture",
+        link: null,
+        date: "1 week ago",
+        relatedImage: "./assets/images/image-chess.webp",
+        message: null,
+        newState: false
+    },
+    {
+        name: "Nathan Peterson",
+        profilePic: './assets/images/avatar-nathan-peterson.webp',
+        reason: "reacted to your recent post",
+        link: "end-game strategies to increase your win rate",
+        date: "2 weeks ago",
+        relatedImage: null,
+        message: null,
+        newState: false
+    },
+    {
+        name: "Anna Kim",
+        profilePic: './assets/images/avatar-anna-kim.webp',
+        reason: "left the group",
+        link: "Chess Club",
+        date: "2 weeks ago",
+        relatedImage: null,
+        message: null,
+        newState: false
+    }
+]
